ba18fcb2eb0780c5671fcf3e327fee901ddee592
Andrew Fischl <andrefisch@github.com>
2016-10-07 21:48:00 -0400

    Jeff's script now works in N dimensions

--- jeffData.py
+++ jeffData.py
@@ -1,5 +1,5 @@
 import math
-DEBUG = True
+DEBUG = False
 
 # how different can you be while still being the same, how similar can you be while still being different?
 # pass an array of tuples, where the first value of each tuple is the group, second is x coordinate and third is y coordinate
@@ -37,12 +37,58 @@ def calcDiff(arr):
 
 # take two tuples from the data in calcDiff arr from above and find the distance between them
 def distance(d0, d1):
-    return math.sqrt((d0[1] - d1[1]) ** 2 + (d0[2] - d1[2]) ** 2)
+    num = 0
+    length = 0
+    # { this can be removed but it ensures that the program will not throw an error if a data point has less dimensions than another datapoint
+    if len(d0) <= len(d1):
+        length = len(d0)
+    else:
+        length = len(d1)
+    # }
+    # distance formula for N dimensions where N is an unknown number
+    for i in range (1, len(d0)):
+        # distance formula in N + 1 steps: loop through N dimensions aggregating the squares of the differences
+        num += (d0[i] - d1[i]) ** 2
+    # then sqrt it all at once at the end
+    return math.sqrt(num)
 
 if DEBUG:
     # answer for this array should be:
     # {Gbberingei: [1.0, 1.0], Gbgrauri: [2.0, 2.0], Gggorilla: [9.0, 1.0]}
-    primateArray = [("Gbberingei", 0, 1), ("Gbberingei", 0, 2), ("Gbgrauri", 0, 4), ("Gbgrauri", 0, 6), ("Gggorilla", 0, 9), ("Gggorilla", 0, 0)]
+    primateArray = [("Gbberingei", 0, 1, 0), ("Gbberingei", 0, 2, 0), ("Gbgrauri", 0, 4, 0), ("Gbgrauri", 0, 6, 0), ("Gggorilla", 0, 9, 0), ("Gggorilla", 0, 0, 0)]
     print "Length of primate test array is", len(primateArray)
     print distance (primateArray[0], primateArray[1])
     print calcDiff(primateArray)
+
+jeffData = [(2,   0.0295245621,    -0.0346388028),
+(2,   -0.1592206205,   -0.1292587769),
+(2,   -0.0279865484,   -0.0102584277),
+(2,   0.0213329541,    -0.0590473527),
+(2,   0.0186482652,    -0.0284362353),
+(2,   -0.0939528258,   0.0560606904),
+(2,   -0.0617759046,   -0.002480195),
+(2,   -0.0221723168,   -0.1453466513),
+(1,   0.0375994251,    0.0185098192),
+(1,   -0.0554840269,   -0.0317779277),
+(1,   0.049232825, -0.0801758889),
+(1,   0.0265247108,    0.0727297007),
+(1,   -0.0051707276,   0.0467700551),
+(1,   -0.1505356262,   0.0023422183),
+(1,   -0.2586979915,   0.0332550498),
+(1,   -0.0712646751,   -0.0759150282),
+(1,   -0.0669691657,   -0.0497186545),
+(1,   -0.1862904045,   0.0632757285),
+(1,   -0.1189739372,   -0.0578387149),
+(3,   0.1621280547,    -0.0541050934),
+(3,   0.2954777406,    0.116909307),
+(3,   0.1178347211,    -0.0528647849),
+(3,   -0.0092498713,   0.0376092302),
+(3,   0.205165106, 0.1666655727),
+(3,   0.1715704139,    -0.0652599082),
+(3,   -0.2059674103,   0.2112276175),
+(3,   -0.0772234853,   0.0841218434),
+(3,   0.2485358622,    0.043009143),
+(3,   0.1873608969,    -0.0753635334)]
+
+print distance(jeffData[0], jeffData[1])
+print calcDiff(jeffData)
