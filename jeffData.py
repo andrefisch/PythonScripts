b599e5e87b734d09ffbb8d275ee94313104f0f55
Andrew Fischl <anfischl@gmail.com>
2016-10-10 22:49:23 -0400

    Restructured jeffData so that it calculates data for individuals instead of one value for each species

--- jeffData.py
+++ jeffData.py
@@ -3,92 +3,120 @@ DEBUG = False
 
 # how different can you be while still being the same, how similar can you be while still being different?
 # pass an array of tuples, where the first value of each tuple is the group, second is x coordinate and third is y coordinate
-# should return an array of tuples where the 
-# - first value of each tuple is how different can you be while still being the same
-# - second value of each tuple is how similar can you be while still being different
+# individuals array stores distance information where the
+# - first value of each array is individual value for how different can you be while still being the same
+# - second value of each array is individual value for how similar can you be while still being different
+# averages dictionary calculates the averages for the species where the
+# - first value of each array is cumulative value of how different can you be while still being the same
+# - second value of each array is cumulative value of how similar can you be while still being different
+# - third value of each array is the number of individuals in the species
 def calcDiff(arr):
-    output = {}
-    # how many species do we have
+    individuals = []
+    averages = {}
     species = []
+    # how many species do we have
+    # this is for the averages for each species
     for i in range (0, len(arr)):
-        if arr[i][0] not in species:
-            species.append(arr[i][0])
-            output[arr[i][0]] = [0, -1]
+        if arr[i][1] not in species:
+            species.append(arr[i][1])
+            # set totals to 0 and species count to 1
+            averages[arr[i][1]] = [0, 0, 1]
+        else:
+            # we are seeing this species again so increment the species count
+            averages[arr[i][1]][2] += 1
     if DEBUG:
-        # check to make sure we have the correct number of output arrays
-        print "Length of output is", len(output)
+        # check to make sure we have the correct number of averages arrays
+        print "Length of averages is", len(averages)
+    # populate the array with baby arrays for 
+    for i in range (0, len(arr)):
+        individuals.append([0, -1])
     for i in range (0, len(arr)):
         for j in range (i, len(arr)):
-            if DEBUG:
-                print "i =", i, "j =", j
             # loop through and test each point against each other point
             temp = distance(arr[i], arr[j])
-            # if two species are the same, save the data if it is greater than greatest recorded data for that species
-            if arr[i][0] == arr[j][0]:
-                if temp > output[arr[i][0]][0]:
-                    output[arr[i][0]][0] = temp
+            if DEBUG:
+                print "i =", i, "j =", j, "distance:", temp
+            # if two species are the same, save the data if it is greater than greatest recorded data for that species and update the intraspecies average and distance
+            if arr[i][1] == arr[j][1]:
+                # update the average for the inner loop species no matter what
+                averages[arr[i][1]][0] += temp
+                if temp > individuals[i][0]:
+                    # update the individual only if its applicable
+                    individuals[i][0] = temp
+                if temp > individuals[j][0]:
+                    # update the individual only if its applicable
+                    individuals[j][0] = temp
             # otherwise they are different so record the value in both of the correct places if applicable
             else:
-                if temp < output[arr[i][0]][1] or output[arr[i][0]][1] < 0:
-                    output[arr[i][0]][1] = temp
-                if temp < output[arr[j][0]][1] or output[arr[j][0]][1] < 0:
-                    output[arr[j][0]][1] = temp
-    return output 
+                # update the average for the inner loop species no matter what
+                averages[arr[i][1]][1] += temp
+                # and update the interspecies distance for one or both species, whatever is applicable 
+                if temp < individuals[i][1] or individuals[i][1] < 0:
+                    individuals[i][1] = temp
+                if temp < individuals[j][1] or individuals[j][1] < 0:
+                    individuals[j][1] = temp
+    if (not DEBUG):
+        printIndividualResults(arr, individuals, averages)
 
 # take two tuples from the data in calcDiff arr from above and find the distance between them
 def distance(d0, d1):
     num = 0
     length = 0
-    # { this can be removed but it ensures that the program will not throw an error if a data point has less dimensions than another datapoint
-    if len(d0) <= len(d1):
-        length = len(d0)
-    else:
-        length = len(d1)
-    # }
     # distance formula for N dimensions where N is an unknown number
-    for i in range (1, len(d0)):
+    # the first number in the range should correspond to the first dimension to be analyzed
+    for i in range (2, len(d0)):
         # distance formula in N + 1 steps: loop through N dimensions aggregating the squares of the differences
         num += (d0[i] - d1[i]) ** 2
     # then sqrt it all at once at the end
     return math.sqrt(num)
 
+def printIndividualResults(arr, individuals, averages):
+    print "Individual Distances"
+    print "      Max Same Species     Min Different Species"
+    for i in range (0, len(individuals)):
+        if (arr[i][0] < 10):
+            print str(arr[i][0]) + ":  ", individuals[i]
+        elif (arr[i][0] < 100):
+            print str(arr[i][0]) + ": ", individuals[i]
+        elif (arr[i][0] < 1000):
+            print str(arr[i][0]) + ":", individuals[i]
+
 if DEBUG:
     # answer for this array should be:
     # {Gbberingei: [1.0, 1.0], Gbgrauri: [2.0, 2.0], Gggorilla: [9.0, 1.0]}
-    primateArray = [("Gbberingei", 0, 1, 0), ("Gbberingei", 0, 2, 0), ("Gbgrauri", 0, 4, 0), ("Gbgrauri", 0, 6, 0), ("Gggorilla", 0, 9, 0), ("Gggorilla", 0, 0, 0)]
+    primateArray = [(0, "Gbberingei", 0, 1, 0), (1, "Gbberingei", 0, 2, 0), (2, "Gbgrauri", 0, 4, 0), (3, "Gbgrauri", 0, 6, 0), (4, "Gggorilla", 0, 9, 0), (5, "Gggorilla", 0, 0, 0)]
     print "Length of primate test array is", len(primateArray)
     print distance (primateArray[0], primateArray[1])
     print calcDiff(primateArray)
 
-jeffData = [(2,   0.0295245621,    -0.0346388028),
-(2,   -0.1592206205,   -0.1292587769),
-(2,   -0.0279865484,   -0.0102584277),
-(2,   0.0213329541,    -0.0590473527),
-(2,   0.0186482652,    -0.0284362353),
-(2,   -0.0939528258,   0.0560606904),
-(2,   -0.0617759046,   -0.002480195),
-(2,   -0.0221723168,   -0.1453466513),
-(1,   0.0375994251,    0.0185098192),
-(1,   -0.0554840269,   -0.0317779277),
-(1,   0.049232825, -0.0801758889),
-(1,   0.0265247108,    0.0727297007),
-(1,   -0.0051707276,   0.0467700551),
-(1,   -0.1505356262,   0.0023422183),
-(1,   -0.2586979915,   0.0332550498),
-(1,   -0.0712646751,   -0.0759150282),
-(1,   -0.0669691657,   -0.0497186545),
-(1,   -0.1862904045,   0.0632757285),
-(1,   -0.1189739372,   -0.0578387149),
-(3,   0.1621280547,    -0.0541050934),
-(3,   0.2954777406,    0.116909307),
-(3,   0.1178347211,    -0.0528647849),
-(3,   -0.0092498713,   0.0376092302),
-(3,   0.205165106, 0.1666655727),
-(3,   0.1715704139,    -0.0652599082),
-(3,   -0.2059674103,   0.2112276175),
-(3,   -0.0772234853,   0.0841218434),
-(3,   0.2485358622,    0.043009143),
-(3,   0.1873608969,    -0.0753635334)]
+jeffData = [(1, 2,   0.0295245621,    -0.0346388028),
+(2,  2,   -0.1592206205,   -0.1292587769),
+(3,  2,   -0.0279865484,   -0.0102584277),
+(4,  2,   0.0213329541,    -0.0590473527),
+(5,  2,   0.0186482652,    -0.0284362353),
+(6,  2,   -0.0939528258,   0.0560606904),
+(7,  2,   -0.0617759046,   -0.002480195),
+(8,  2,   -0.0221723168,   -0.1453466513),
+(9,  1,   0.0375994251,    0.0185098192),
+(10, 1,   -0.0554840269,   -0.0317779277),
+(11, 1,   0.049232825, -0.0801758889),
+(12, 1,   0.0265247108,    0.0727297007),
+(13, 1,   -0.0051707276,   0.0467700551),
+(14, 1,   -0.1505356262,   0.0023422183),
+(15, 1,   -0.2586979915,   0.0332550498),
+(16, 1,   -0.0712646751,   -0.0759150282),
+(17, 1,   -0.0669691657,   -0.0497186545),
+(18, 1,   -0.1862904045,   0.0632757285),
+(19, 1,   -0.1189739372,   -0.0578387149),
+(20, 3,   0.1621280547,    -0.0541050934),
+(21, 3,   0.2954777406,    0.116909307),
+(22, 3,   0.1178347211,    -0.0528647849),
+(23, 3,   -0.0092498713,   0.0376092302),
+(24, 3,   0.205165106, 0.1666655727),
+(25, 3,   0.1715704139,    -0.0652599082),
+(26, 3,   -0.2059674103,   0.2112276175),
+(27, 3,   -0.0772234853,   0.0841218434),
+(28, 3,   0.2485358622,    0.043009143),
+(29, 3,   0.1873608969,    -0.0753635334)]
 
-print distance(jeffData[0], jeffData[1])
-print calcDiff(jeffData)
+calcDiff(jeffData)
